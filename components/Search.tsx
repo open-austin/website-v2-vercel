92a5ae5535efcd6a5bd1375b6c009e5cf6ca91e9
attali-david <dattali93@gmail.com>
2022-10-21 15:17:51 -0500

    optimized search function

--- components/Search.tsx
+++ components/Search.tsx
@@ -2,28 +2,28 @@ import { useEffect, useState, useRef } from 'react'
 import { default as projectData } from '../data/project_data'
 import { MultiValue } from 'chakra-react-select'
 import SearchBar from './SearchBar'
+import { Projects } from '../types/Projects'
 
 const UNKNOWN_CATEGORY = 'unknown'
 
 export type SelectedMulti = Array<{ value: string; label: string }>
 
-interface Props {
-  setCurrentCategory: (currentCategory: Set<string>) => void
-  setCurrentStatus: (currentStatus: Set<string>) => void
+type Props = {
+  setProjectList: (projectList: Projects[]) => void
 }
 
 const useSearchBar = () => {
   const [categories, setCategories] = useState<SelectedMulti>([])
   const [statuses, setStatuses] = useState<SelectedMulti>([])
+  const [categorySet, setCategorySet] = useState<string[]>([])
   const isMounted = useRef(true)
 
   useEffect(() => {
     if (isMounted.current) {
-      const cats = Array.from(
-        new Set(projectData.flatMap((p) => p.categories))
-      ).map((label) => ({ value: label, label }))
-
-      setCategories(cats)
+      const cats = Array.from(new Set(projectData.flatMap((p) => p.categories)))
+      let catSelectedMulti = cats.map((label) => ({ value: label, label }))
+      setCategorySet(cats)
+      setCategories(catSelectedMulti)
 
       const stats = Array.from(
         new Set(projectData.flatMap((p) => p.status || UNKNOWN_CATEGORY))
@@ -37,11 +37,63 @@ const useSearchBar = () => {
   return {
     categories,
     statuses,
+    categorySet,
   }
 }
 
-const Search = ({ setCurrentCategory, setCurrentStatus }: Props) => {
-  const { categories, statuses } = useSearchBar()
+const Search = ({ setProjectList }: Props) => {
+  const { categories, statuses, categorySet } = useSearchBar()
+  const [currentCategory, setCurrentCategory] = useState<Set<string>>(new Set())
+  const [currentStatus, setCurrentStatus] = useState<Set<string>>(new Set())
+
+  function projectFilter() {
+    // Handles clearing multiselectors
+    if (currentCategory.size === 0 && currentStatus.size === 0) {
+      setProjectList(projectData)
+    }
+    // Handles multiselectors
+    else {
+      /*       
+        Creates a hash of projects for every category and status.
+        Updates searchedProjects state by retrieving values for each entry of currentCategory or currentStatus or both.
+      */
+      let searchedProjects: Projects[] = []
+      let selectorHash = new Map()
+
+      categorySet.forEach((cat) => selectorHash.set(cat, []))
+      for (let p of projectData) {
+        const { categories, status } = p
+        selectorHash.has(status)
+          ? selectorHash.get(status).push(p)
+          : selectorHash.set(status, [p])
+
+        categories.forEach((cat) => selectorHash.get(cat).push(p))
+      }
+
+      if (currentStatus.size == 0 && currentCategory.size > 0) {
+        currentCategory.forEach((cat) => {
+          searchedProjects.push(selectorHash.get(cat))
+        })
+      } else if (currentStatus.size > 0 && currentCategory.size == 0) {
+        currentStatus.forEach((stat) =>
+          searchedProjects.push(selectorHash.get(stat))
+        )
+      } else {
+        let categoryFilter: Projects[] = []
+        currentCategory.forEach((cat) =>
+          categoryFilter.push(selectorHash.get(cat))
+        )
+        searchedProjects = categoryFilter.filter((cat) =>
+          currentStatus.has(cat.status)
+        )
+      }
+      setProjectList(searchedProjects.flat())
+    }
+  }
+
+  useEffect(() => {
+    projectFilter()
+  }, [currentCategory, currentStatus])
 
   const selectionHandler = (
     selection: MultiValue<{

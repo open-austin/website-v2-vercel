aecd8c4c2386e0838eabc8b2ea07fecc0a811eb8
Liani Lye <lye.liani@gmail.com>
2022-12-12 13:50:27 -0600

    Update project_data.ts

--- data/project_data.ts
+++ data/project_data.ts
@@ -181,7 +181,7 @@ export const projectData = [
     contact: ['Dan Minshew', '@newswim'],
     date: '2019-03-14T00:00:00.000Z',
     thumb: null,
-    screenshot: null,
+    screenshot: `ballotapi.jpg`,
     featured: false,
     stack: ['Python', 'R', 'Azure Functions', `TypeScript`, `React`, `D3`],
     skipSize: 496,

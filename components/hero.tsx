d902a9c2ee6e3a78f53c221c3bd0dad0e34d3d97
newswim <ofenixculpa@gmail.com>
2023-01-15 15:11:40 -0600

    (fix) hero mobile view

--- components/hero.tsx
+++ components/hero.tsx
@@ -13,7 +13,7 @@ import {
 import { Link } from './link'
 
 const Hero = () => (
-  <Flex w={'100vw'} h={'calc(100vh - 335px)'} pos="relative">
+  <Flex minH={'calc(100vh - 335px)'} pos="relative">
     <Image
       priority
       alt="Gathering of volunteers at an Open Austin event"
@@ -26,67 +26,57 @@ const Hero = () => (
       w={'full'}
       zIndex={1}
       justify={'center'}
-      px={useBreakpointValue({ base: 4, md: 8 })}
       bgGradient={'linear(to-r, blackAlpha.600, transparent)'}
     >
-      <Stack
-        maxW={'3xl'}
-        align={'flex-start'}
-        spacing={6}
-        justifyContent="center"
-        alignItems={'center'}
+      <Box
+        boxShadow="2xl"
+        maxW={useBreakpointValue({ base: 'full', md: 600 })}
+        p={useBreakpointValue({ base: 4, md: 12 })}
+        rounded="md"
+        bgColor={'blackAlpha.300'}
       >
-        <Box
-          boxShadow="2xl"
-          p="12"
-          rounded="md"
-          bg="white"
-          bgColor={'blackAlpha.300'}
+        <Heading
+          as="h1"
+          color={'orange.300'}
+          textAlign="center"
+          fontSize={useBreakpointValue({ base: '6xl', md: '7xl' })}
+          fontWeight={900}
         >
-          <Heading
-            as="h1"
-            color={'orange.300'}
-            textAlign="center"
-            fontSize={useBreakpointValue({ base: '6xl', md: '7xl' })}
-            fontWeight={900}
-          >
-            Open Austin
-          </Heading>
-          <Text
-            color={'gray.200'}
-            align="center"
-            fontWeight={500}
-            lineHeight={1.2}
-            p={4}
-            fontSize={useBreakpointValue({ base: 'xl', md: 'lg' })}
-          >
-            Open Austin addresses local social and civic challenges through
-            creative uses of technology. We foster relationships between
-            government, non- and for-profit organizations, and resident
-            activists.
-          </Text>
+          Open Austin
+        </Heading>
+        <Text
+          color={'gray.200'}
+          align="center"
+          fontWeight={500}
+          lineHeight={1.2}
+          p={4}
+          fontSize={useBreakpointValue({ base: 'xl', md: 'lg' })}
+        >
+          Open Austin addresses local social and civic challenges through
+          creative uses of technology. We foster relationships between
+          government, non- and for-profit organizations, and resident activists.
+        </Text>
 
-          <Heading
-            as="h2"
-            color={'gray.100'}
-            textAlign="center"
-            fontSize={useBreakpointValue({ base: '3xl', md: '5xl' })}
-            fontWeight={700}
-            pb={4}
-          >
-            All are welcome!
-          </Heading>
+        <Heading
+          as="h2"
+          color={'gray.100'}
+          textAlign="center"
+          fontSize={useBreakpointValue({ base: '3xl', md: '5xl' })}
+          fontWeight={700}
+          pb={4}
+        >
+          All are welcome!
+        </Heading>
 
-          <Stack direction={'row'} align={'flex-end'} justifyContent="center">
-            <Link href="about">
-              <Button variant="primary">About</Button>
-            </Link>
-            <Link href="portfolio">
-              <Button variant="primary">Projects</Button>
-            </Link>
-          </Stack>
-        </Box>
-      </Stack>
+        <Stack direction={'row'} align={'flex-end'} justifyContent="center">
+          <Link href="about">
+            <Button variant="primary">About</Button>
+          </Link>
+          <Link href="portfolio">
+            <Button variant="primary">Projects</Button>
+          </Link>
+        </Stack>
+      </Box>
     </VStack>
   </Flex>
 )

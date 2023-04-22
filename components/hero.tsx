31800372c3443f42a10b6ccffa03405e756670e2
James Calcaben <jcalcaben+github@gmail.com>
2023-04-22 03:06:06 -0500

    Fix layout shift caused by useBreakpointValue() hook

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 import {
   Box,
   Button,
@@ -8,49 +8,48 @@ import {
   Flex,
   Text,
   VStack,
-  useBreakpointValue,
   Heading,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 
 const Hero = () => (
-  <Flex minH={'calc(100vh - 335px)'} pos="relative">
+  <Flex minH={"calc(100vh - 335px)"} pos="relative">
     <Image
       priority
       alt="Gathering of volunteers at an Open Austin event"
-      src={'/assets/code-across-2015-pano.png'}
+      src={"/assets/code-across-2015-pano.png"}
       layout="fill"
       objectFit="cover"
       quality={80}
     />
     <VStack
-      w={'full'}
+      w={"full"}
       zIndex={1}
-      justify={'center'}
-      bgGradient={'linear(to-r, blackAlpha.600, transparent)'}
+      justify={"center"}
+      bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
     >
       <Box
         boxShadow="2xl"
-        maxW={useBreakpointValue({ base: 'full', md: 600 })}
-        p={useBreakpointValue({ base: 4, md: 12 })}
+        maxW={["full", 600]}
+        p={[4, 12]}
         rounded="md"
-        bgColor={'blackAlpha.300'}
+        bgColor={"blackAlpha.300"}
       >
         <Heading
           as="h1"
-          color={'orange.300'}
+          color={"orange.300"}
           textAlign="center"
-          fontSize={useBreakpointValue({ base: '6xl', md: '7xl' })}
+          fontSize={["6xl", "7xl"]}
           fontWeight={900}
         >
           Open Austin
         </Heading>
         <Text
-          color={'gray.200'}
+          color={"gray.200"}
           align="center"
           fontWeight={500}
           lineHeight={1.2}
           p={4}
-          fontSize={useBreakpointValue({ base: 'xl', md: 'lg' })}
+          fontSize={["xl", "lg"]}
         >
           Open Austin addresses local social and civic challenges through
           creative uses of technology. We foster relationships between
@@ -59,26 +58,30 @@ const Hero = () => (
 
         <Heading
           as="h2"
-          color={'gray.100'}
+          color={"gray.100"}
           textAlign="center"
-          fontSize={useBreakpointValue({ base: '3xl', md: '5xl' })}
+          fontSize={["3xl", "5xl"]}
           fontWeight={700}
           pb={4}
         >
           All are welcome!
         </Heading>
 
-        <Stack direction={'row'} align={'flex-end'} justifyContent="center">
+        <Stack direction={"row"} align={"flex-end"} justifyContent="center">
           <Link href="about" passHref>
-            <Button variant="primary" as="a">About</Button>
+            <Button variant="primary" as="a">
+              About
+            </Button>
           </Link>
           <Link href="portfolio" passHref>
-            <Button variant="primary" as="a">Projects</Button>
+            <Button variant="primary" as="a">
+              Projects
+            </Button>
           </Link>
         </Stack>
       </Box>
     </VStack>
   </Flex>
-)
+);
 
-export default Hero
+export default Hero;

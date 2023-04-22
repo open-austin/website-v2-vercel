b29bdb4e05d248f17c22e7935cd3a65d7b376264
James Calcaben <jcalcaben+github@gmail.com>
2023-04-22 03:05:25 -0500

    Fix font flashing bug by pre-loading font

--- next.config.js
+++ next.config.js
@@ -3,4 +3,17 @@ module.exports = {
   reactStrictMode: true,
   // exportPathMap: () => {}
   swcMinify: true,
-}
+  webpack: (config) => {
+    config.module = {
+      ...config.module,
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.(woff|woff2)$/,
+          type: 'asset/resource'
+        },
+      ],
+    };
+    return config;
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 // Importing fonts (there's probably a better way to do this...)
-import '@fontsource/crimson-text'
+
+import "@fontsource/crimson-text";
+import crimsonTextLatin from "@fontsource/crimson-text/files/crimson-text-latin-400-normal.woff2";
 
 // Setup for Chakra and Next environments
-import { ChakraProvider } from '@chakra-ui/react'
-import { Analytics } from '@vercel/analytics/react'
+import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 
-import { type AppProps } from 'next/app'
-import Head from 'next/head'
+import { type AppProps } from "next/app";
+import Head from "next/head";
 
-import { theme } from '../lib/theme'
-import { Header } from '../components/header'
-import { Footer } from '../components/footer'
+import { theme } from "../lib/theme";
+import { Header } from "../components/header";
+import { Footer } from "../components/footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -22,13 +24,20 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="Open Austin is a volunteer citizen brigade advocating for open government, open data, and civic application development since 2009."
         />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="preload"
+          href={crimsonTextLatin}
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
       </Head>
       <Header />
       <Analytics />
       <Component {...pageProps} />
       <Footer />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;

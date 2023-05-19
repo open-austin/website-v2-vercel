bcaffe26fa4aeb1386b6c4d3c5a55892c217190e
James Calcaben <jcalcaben+github@gmail.com>
2023-05-18 20:26:39 -0500

    Run prettier on _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
 // Importing fonts (there's probably a better way to do this...)
 
-import "@fontsource/crimson-text";
-import crimsonTextLatin from "@fontsource/crimson-text/files/crimson-text-latin-400-normal.woff2";
+import '@fontsource/crimson-text'
+import crimsonTextLatin from '@fontsource/crimson-text/files/crimson-text-latin-400-normal.woff2'
 
 // Setup for Chakra and Next environments
-import { ChakraProvider } from "@chakra-ui/react";
-import { Analytics } from "@vercel/analytics/react";
+import { ChakraProvider } from '@chakra-ui/react'
+import { Analytics } from '@vercel/analytics/react'
 
-import { type AppProps } from "next/app";
-import Head from "next/head";
+import { type AppProps } from 'next/app'
+import Head from 'next/head'
 
-import { theme } from "../lib/theme";
-import { Header } from "../components/header";
-import { Footer } from "../components/footer";
+import { theme } from '../lib/theme'
+import { Header } from '../components/header'
+import { Footer } from '../components/footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -37,7 +37,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
       <Footer />
     </ChakraProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
